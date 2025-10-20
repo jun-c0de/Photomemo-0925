f4524029bd997937ee548ad53ff636a7a4b355cf
jun-c0de <sh48520414@gmail.com>
2025-10-20 20:41:17 +0900

    cc

---
+++ frontend/src/pages/user/UserDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FileList from './FileList'
+import UploadForm from './UploadForm'
+
+const UserDashboard = () => {
+    return (
+        <section>
+            <FileList/>
+            <UploadForm/>
+        </section>
+    )
+}
+
+export default UserDashboard
